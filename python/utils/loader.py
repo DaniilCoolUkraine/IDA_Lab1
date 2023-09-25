a5b5931a3d9071ae1bd1bf9a4d2906e949b1ee8b
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2023-09-25 18:54:41 +0300

    The second block of task has been completed

--- python/utils/loader.py
+++ python/utils/loader.py
@@ -1,19 +1,15 @@
+import pandas as pd
 import requests as req
 from colorama import Fore as text_color
 from colorama import Style as text_style
-from sys import platform
 from os import path
 
 
 class Loader:
 	@staticmethod
-	def send_get_request():
+	def send_get_request(file_path: str):
 		download_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/nba-elo/nbaallelo.csv"
-		if platform == 'linux':
-			target_csv_path = "dataset/nba.csv"
-		else:
-			target_csv_path = "python/dataset/nba.csv"
-
+		target_csv_path = file_path + 'nba.csv'
 		if path.exists(target_csv_path):
 			return
 
